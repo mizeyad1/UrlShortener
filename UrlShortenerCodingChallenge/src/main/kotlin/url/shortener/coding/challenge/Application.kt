01fc5104d663cf55c6763d3d2fcb0a4214e3fa3f
mizeyad1 <zeyad.a.sharaf@gmail.com>
2025-01-21 21:44:31 +0100

    Initial Commit , setting up the project

---
+++ UrlShortenerCodingChallenge/src/main/kotlin/url/shortener/coding/challenge/Application.kt
@@ -0,0 +1,11 @@
+package url.shortener.coding.challenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
---
+++ UrlShortenerCodingChallenge/src/test/kotlin/url/shortener/coding/challenge/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package url.shortener.coding.challenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
